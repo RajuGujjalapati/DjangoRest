f19be6379c3a4a2ae48fe9977892e4a7bad80ed8
Rajulearner <gujjalapatiraju@gmail.com>
2021-02-03 22:06:53 +0530

    serializers(model, hyperlinked)

---
+++ djangorestbasics/djangorestpro/myrestapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import LanguageSerializer
+from .models import Language
+
+# Create your views here.
+
+class LanguageView(viewsets.ModelViewSet):
---
+++ djangorestpro/myrestapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import LanguageSerializer
+from .models import Language
+
+# Create your views here.
+
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
